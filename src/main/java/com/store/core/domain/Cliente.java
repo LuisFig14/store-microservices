2ef7b853385cdd78e7d2c752a7183ff42c3e5039
Fernando Licea <fernandolic1407@gmail.com>
2024-04-10 07:32:22 -0600

    Entity modified

--- src/main/java/com/store/core/domain/Cliente.java
+++ src/main/java/com/store/core/domain/Cliente.java
@@ -1,46 +1,18 @@
 package com.store.core.domain;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cliente {
 
     //Attributes
     private String username;
     private String password;
     private String nombre;
-
-
-    //Constructor
-    public Cliente (String username, String password, String nombre){
-        this.username = username;
-        this.password = password;
-        this.nombre = nombre;
-    }
-
-
-    //getter and setter
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
 
 }
--- src/main/java/com/store/core/domain/Producto.java
+++ src/main/java/com/store/core/domain/Producto.java
@@ -1,6 +1,13 @@
 package com.store.core.domain;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Producto {
 
     //Attributes
@@ -8,46 +15,5 @@ public class Producto {
     private String nombre;
     private Double precio;
     private Integer stock;
-
-
-    //Constructor
-    public Producto(Integer id, String nombre, Double precio, Integer stock){
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.stock = stock;
-    }
-
-    public Producto(){
-
-    }
-
-     public Integer getId() {
-         return id;
-     }
-
-     public String getNombre(){
-        return this.nombre;
-     }
-
-     public Double getPrecio(){
-        return this.precio;
-     }
-
-     public Integer getStock(){
-        return stock;
-     }
-
-     //Setters
-    public void setId(){this.id = id; }
-
-    public void setNombre(){this.nombre = nombre; }
-    public void setPrecio(){this.precio = precio;}
-    public void setStock(){this.stock= stock; }
-
-
-
-
-
 
 }
